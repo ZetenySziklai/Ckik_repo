e1638697b9c72185bac967878a0581eb37755c96
Sziklai Zétény <sziklai.zeteny@ckik.loc>
2024-11-07 14:10:11 +0100

    black

---
+++ Szoftvertesztelés/2024_11_07_TesztelesAlapjai/2024_11_07_TesztelesAlapjai/Szamelmelet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_11_07_TesztelesAlapjai
+{
+    internal class Szamelmelet
+    {
+        /// <summary>
+        /// Számelméleti algoritmusok
+        /// </summary>
+        int a;
+        public Szamelmelet(int uja)
+        {
+            a = uja;
+        }
+
+        /// <summary>
+        /// A függvény megtuplázza az osztályban használz "a" változó értékét!
+        /// </summary>
+        /// <returns>Megduplázott érték</returns>
+        public int Duplazas()
+        {
+            return a * 2;
+        }
+
+        public double Hatvanyozas(int kitevo)
+        {
+            double szam = 1;
+            for (int i = 0; i < kitevo; i++) 
+            {
+                szam = szam * a;
+            }
+            return szam;
+            
+            
+
+
+        }
+    }
+}
